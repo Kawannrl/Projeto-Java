1a492a5e56113b2727755694b59fc9aa29d665b2
Kawannrl <165685845+Kawannrl@users.noreply.github.com>
2024-10-10 14:46:18 -0300

    Update papel.java

--- Atividade 04.java/papel.java
+++ Atividade 04.java/papel.java
@@ -2,24 +2,24 @@ package Atividade04.java;
 
 import java.util.Random;
 
-public class papel {
-    public static int papel (){
+public class pedra {
+    public static int pedra (){
 
         Random random = new Random ();
 
         int aleatorio = random.nextInt (3) + 1;
-        int escolha = 2;
+        int escolha = 1;
 
-        if (aleatorio == 1){
-            System.out.println ("||   VITÓRIA   ||");
-            System.out.println ("O adversário escolheu PEDRA");
-        }
-        else if (aleatorio == escolha){
+        if (aleatorio == escolha){
             System.out.println ("||   EMPATE   ||");
-            System.out.println ("Ambos escolheram PAPEL");
+            System.out.println ("Ambos escolheram PEDRA");
+        }
+        else if (aleatorio == 2){
+            System.out.println ("||   DERROTA   ||");
+            System.out.println ("O adversário escolheu PAPEL");
         }
         else {
-            System.out.println ("||   DERROTA  ||");
+            System.out.println ("||   VITÓRIA   ||");
             System.out.println ("O adversário escolheu TESOURA");
         }
         return 0;
