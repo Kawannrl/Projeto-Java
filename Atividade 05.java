ec7469abc48f2198ad7a02fd18eaa6cd736c61e5
Kawannrl <165685845+Kawannrl@users.noreply.github.com>
2024-10-15 14:58:05 -0300

    Create Atividade 05.java

---
+++ Atividade 05.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main (String[] args){
+
+        int num = 5, resultado = 0;
+
+        for (int i = 0; i <= 5; i++){
+            resultado += 5 * (num - 1);
+        }
+        System.out.println (resultado);
+    }
+}
