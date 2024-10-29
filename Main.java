ee28a4b193b41e2adfefb2f9f2f98b0ef5e74b3b
Kawannrl <165685845+Kawannrl@users.noreply.github.com>
2024-10-29 14:31:36 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main (String [] args){
-        PagamentoBoleto PagamentoBoleto1 = new PagamentoBoleto (5646.0, "563462");
-        PagamentoCartao PagamentoCartao1 = new PagamentoCartao (45645.0, "3657535");
-
-        PagamentoBoleto1.realizar_pagamento ();
-        PagamentoCartao1.realizar_pagamento ();
-    }
-}
