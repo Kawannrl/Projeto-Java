02210870429aa09479e14a766d5aed6a73987dcd
Kawannrl <165685845+Kawannrl@users.noreply.github.com>
2024-11-05 17:25:27 -0300

    Add files via upload

---
+++ Atividade 17.java/Array2__2.java
@@ -0,0 +1,9 @@
+public class Array2__2 {
+    public static void main (String [] args){
+        int vetor [] = {1,2,3,4,5,6,7,8,9,10};
+
+        for (int i = 9; i >= 0; i--){
+            System.out.println (vetor [i]);
+        }
+    }
+}
