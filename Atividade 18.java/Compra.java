a79bc0177964f96681e7703f7dd949da30fa30fc
Kawannrl <165685845+Kawannrl@users.noreply.github.com>
2024-11-18 17:12:31 -0300

    Update Compra.java

--- Atividade 18.java/Compra.java
+++ Atividade 18.java/Compra.java
@@ -10,22 +10,27 @@ public class Compra {
 
         System.out.println ("Informe em qual andar você quer ficar: \n1 - 1° Andar\n2 - 2° Andar\n3 - 3° Andar");
         andar = scanner.nextInt ();
-        System.out.println ("Informe eo assento escolhido: ");
-        System.out.print ("Linha: ");
-        linha = scanner.nextInt ();
-        System.out.print ("Coluna: ");
-        coluna = scanner.nextInt ();
-        System.out.println ();
 
-        andar--;
-        coluna--;
-        linha--;
+        if (andar >= 1 || andar <= 3){
+            System.out.println("Informe o assento escolhido: ");
+            System.out.print("Linha: ");
+            linha = scanner.nextInt();
+            System.out.print("Coluna: ");
+            coluna = scanner.nextInt();
+            System.out.println();
 
-        Status.comprar (andar, linha, coluna);
+            andar--;
 
-        System.out.println ();
-        System.out.println ("O que deseja agora? ");
-        System.out.println ();
-        Menu.menu ();
+            Status.comprar(andar, linha, coluna);
+
+            System.out.println();
+            System.out.println("O que deseja agora? ");
+            System.out.println();
+            Menu.menu();
+        }
+        else {
+            System.out.println ("Erro!!!");
+            System.out.println ("Escolha um número de 1 á 3!");
+        }
     }
 }
