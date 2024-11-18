ba5402f562f72e72a4ce9bce4753dded46d52de7
Kawannrl <165685845+Kawannrl@users.noreply.github.com>
2024-11-18 17:12:56 -0300

    Update Desistencia.java

--- Atividade 18.java/Desistencia.java
+++ Atividade 18.java/Desistencia.java
@@ -10,11 +10,27 @@ public class Desistencia {
 
         System.out.println ("Informe o andar do assento que será devolvido: \n1 - 1° Andar\n2 - 2° Andar\n3 - 3° Andar");
         andar = scanner.nextInt ();
-        System.out.println ("Informe eo assento escolhido: ");
-        System.out.print ("Linha: ");
-        linha = scanner.nextInt ();
-        System.out.print ("Coluna: ");
-        coluna = scanner.nextInt ();
-        System.out.println ();
+
+        if (andar >= 1 || andar <= 3) {
+            System.out.println("Informe o assento escolhido: ");
+            System.out.print("Linha: ");
+            linha = scanner.nextInt();
+            System.out.print("Coluna: ");
+            coluna = scanner.nextInt();
+            System.out.println();
+
+            andar--;
+
+            Status.cancelar(andar, linha, coluna);
+
+            System.out.println();
+            System.out.println("O que deseja agora? ");
+            System.out.println();
+            Menu.menu();
+        }
+        else {
+            System.out.println ("Erro!!!");
+            System.out.println ("Escolha um número de 1 á 3!");
+        }
     }
 }
