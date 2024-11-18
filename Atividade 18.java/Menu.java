3cc110546c012283939b311bdf44bd88adbe3007
Kawannrl <165685845+Kawannrl@users.noreply.github.com>
2024-11-18 17:12:04 -0300

    Update Menu.java

--- Atividade 18.java/Menu.java
+++ Atividade 18.java/Menu.java
@@ -8,6 +8,7 @@ public class Menu {
 
         do {
             System.out.println("Escolha uma das opções: \n1 - Comprar Ingresso\n2 - Cancelar Ingresso\n3 - Assentos\n4 - Histórico de Compras\n5 - Sair");
+            System.out.print ("Resposta: ");
             opcao = scanner.nextInt();
 
             switch (opcao){
@@ -21,11 +22,16 @@ public class Menu {
                     Status.status ();
                     break;
                 case 4:
+                    Logs.imprimir ();
                     break;
                 case 5:
                     System.out.println ("O programa está sndo encerrado pelo usuário!");
                     System.out.println ("Encerrando...");
                     System.exit (0);
+                    break;
+                default:
+                    System.out.println ("Erro!!!");
+                    System.out.println ("Escolha uma opção de 1 á 5!");
             }
         } while (opcao != 1 && opcao != 2 && opcao != 3 && opcao != 4 && opcao != 5);
     }
