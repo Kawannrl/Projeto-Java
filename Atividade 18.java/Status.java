2b903a2ba71b2ccabac2c0a2ece596aa68f00ea6
Kawannrl <165685845+Kawannrl@users.noreply.github.com>
2024-11-19 17:13:19 -0300

    Update Status.java

--- Atividade 18.java/Status.java
+++ Atividade 18.java/Status.java
@@ -11,49 +11,53 @@ public class Status {
         System.out.println ("Informe em qual andar você quer ficar: \n1 - 1° Andar\n2 - 2° Andar\n3 - 3° Andar\n");
         andar = scanner.nextInt ();
 
-        if (andar == 1){
-            for (int i = 1; i < 31; i++){
-                System.out.print (i + "A  ");
-                for (int j = 1; j < 51; j++){
-                    if (assento [i][j][0] == false){
-                        System.out.print (j + " - L \t");
-                    } else {
-                        System.out.print (j + " - V \t");
+        do {
+            if (andar == 1) {
+                for (int i = 1; i < 31; i++) {
+                    System.out.print(i + "A  ");
+                    for (int j = 1; j < 51; j++) {
+                        if (assento[i][j][0] == false) {
+                            System.out.print(j + " - L \t");
+                        } else {
+                            System.out.print(j + " - V \t");
+                        }
                     }
+                    System.out.println();
                 }
-                System.out.println ();
             }
-        }
 
-        if (andar == 2){
-            for (int i = 1; i < 21; i++){
-                System.out.print (i + "A  ");
-                for (int j = 1; j < 31; j++){
-                    if (assento [i][j][1] == false){
-                        System.out.print (j + " - L \t");
-                    } else {
-                        System.out.print (j + " - V \t");
+            if (andar == 2) {
+                for (int i = 1; i < 21; i++) {
+                    System.out.print(i + "A  ");
+                    for (int j = 1; j < 31; j++) {
+                        if (assento[i][j][1] == false) {
+                            System.out.print(j + " - L \t");
+                        } else {
+                            System.out.print(j + " - V \t");
+                        }
                     }
+                    System.out.println();
                 }
-                System.out.println ();
             }
-        }
 
-        if (andar == 3){
-            for (int i = 1; i < 11; i++){
-                System.out.print (i + "\t");
-                for (int j = 1; j < 16; j++){
-                    if (assento [i][j][2] == false){
-                        System.out.print (j + " - L \t");
-                    } else {
-                        System.out.print (j + " - V \t");
+            if (andar == 3) {
+                for (int i = 1; i < 11; i++) {
+                    System.out.print(i + "\t");
+                    for (int j = 1; j < 16; j++) {
+                        if (assento[i][j][2] == false) {
+                            System.out.print(j + " - L \t");
+                        } else {
+                            System.out.print(j + " - V \t");
+                        }
                     }
+                    System.out.println();
                 }
-                System.out.println ();
             }
+            System.out.println();
+            Menu.menu();
         }
-        System.out.println ();
-        Menu.menu ();
+        while (andar != 1 && andar != 2 && andar != 3);
+        Erros.andar_invalido ("Andar inválido");
     }
 
     public static void comprar (int andar, int linha, int coluna){
