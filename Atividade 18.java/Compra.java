53f2e1b3ac6439b23456c68e073bbc5ce7be27ae
Kawannrl <165685845+Kawannrl@users.noreply.github.com>
2024-11-22 16:56:07 -0300

    Update Compra.java

--- Atividade 18.java/Compra.java
+++ Atividade 18.java/Compra.java
@@ -9,33 +9,93 @@ public class Compra {
         int andar = 0;
 
         do {
+            System.out.println ();
             System.out.println ("Informe em qual andar você quer ficar: \n1 - 1° Andar\n2 - 2° Andar\n3 - 3° Andar");
             andar = scanner.nextInt ();
-            System.out.println("Informe o assento escolhido: ");
 
-            System.out.print("Linha: ");
-            linha = scanner.nextInt();
-
-            do {
-                System.out.print("Coluna: ");
-                coluna = scanner.nextInt();
-                System.out.println();
-
-                andar--;
-
-                Status.comprar(andar, linha, coluna);
-
-                System.out.println();
-                System.out.println("O que deseja agora? ");
-                System.out.println();
-                Menu.menu();
+            if (andar != 1 && andar != 2 && andar != 3){
+                Erros.andar_invalido ("Andar inválido");
             }
-            while (linha >= 1 && linha <= 50);
-
-            Erros.linha_invalida ("Linha inválida");
         }
         while (andar != 1 && andar != 2 && andar != 3);
 
-        Erros.andar_invalido ("Andar inválido");
+        if (andar == 1){
+
+            do {
+                System.out.println ("Informe o assento escolhido: ");
+                System.out.print ("Linha: ");
+                linha = scanner.nextInt ();
+
+                if (linha < 1 || linha > 30){
+                    Erros.linha_invalida ("Linha inválida");
+                }
+            }
+            while (linha < 1 || linha > 30);
+
+            do {
+                System.out.print ("Coluna: ");
+                coluna = scanner.nextInt ();
+
+                if (coluna < 1 || coluna > 50){
+                    Erros.coluna_invalida ("Coluna inválida");
+                }
+            }
+            while (coluna < 1 || coluna > 50);
+        }
+
+        else if (andar == 2){
+            do {
+                System.out.println ("Informe o assento escolhido: ");
+                System.out.print ("Linha: ");
+                linha = scanner.nextInt ();
+
+                if (linha < 1 || linha > 20){
+                    Erros.linha_invalida ("Linha inválida");
+                }
+            }
+            while (linha < 1 || linha > 20);
+
+            do {
+                System.out.print ("Coluna: ");
+                coluna = scanner.nextInt ();
+
+                if (coluna < 1 || coluna > 30){
+                    Erros.coluna_invalida ("Coluna inválida");
+                }
+            }
+            while (coluna < 1 || coluna > 30);
+        }
+
+        else {
+            do {
+                System.out.println ("Informe o assento escolhido: ");
+                System.out.print ("Linha: ");
+                linha = scanner.nextInt ();
+
+                if (linha < 1 || linha > 10){
+                    Erros.linha_invalida ("Linha inválida");
+                }
+            }
+            while (linha < 1 || linha > 10);
+
+            do {
+                System.out.print ("Coluna: ");
+                coluna = scanner.nextInt ();
+
+                if (coluna < 1 || coluna > 15){
+                    Erros.coluna_invalida ("Coluna inválida");
+                }
+            }
+            while (coluna < 1 || coluna > 15);
+        }
+
+        andar--;
+
+        Status.comprar (andar, linha, coluna);
+
+        System.out.println ();
+        System.out.println ("O que deseja agora? ");
+        System.out.println ();
+        Menu.menu ();
     }
 }
