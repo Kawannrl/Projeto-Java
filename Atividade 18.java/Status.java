ffe8ae16d06539a19b4fd35062f5cc51c41fc1f4
Kawannrl <165685845+Kawannrl@users.noreply.github.com>
2024-11-22 16:55:52 -0300

    Update Status.java

--- Atividade 18.java/Status.java
+++ Atividade 18.java/Status.java
@@ -8,81 +8,88 @@ public class Status {
 
         int andar = 0;
 
-        System.out.println ("Informe em qual andar você quer ficar: \n1 - 1° Andar\n2 - 2° Andar\n3 - 3° Andar\n");
-        andar = scanner.nextInt ();
-
         do {
-            if (andar == 1) {
-                for (int i = 1; i < 31; i++) {
-                    System.out.print(i + "A  ");
-                    for (int j = 1; j < 51; j++) {
-                        if (assento[i][j][0] == false) {
-                            System.out.print(j + " - L \t");
+            System.out.println ();
+            System.out.println ("Informe em qual andar você quer ficar: \n1 - 1° Andar\n2 - 2° Andar\n3 - 3° Andar\n");
+            andar = scanner.nextInt ();
+
+            if (andar == 1){
+                for (int i = 1; i < 31; i++){
+                    System.out.print (i + "A  ");
+                    for (int j = 1; j < 51; j++){
+                        if (assento [i][j][0] == false){
+                            System.out.print (j + " - L \t");
                         } else {
-                            System.out.print(j + " - V \t");
+                            System.out.print (j + " - V \t");
                         }
                     }
-                    System.out.println();
+                    System.out.println ();
                 }
             }
 
-            if (andar == 2) {
-                for (int i = 1; i < 21; i++) {
-                    System.out.print(i + "A  ");
-                    for (int j = 1; j < 31; j++) {
-                        if (assento[i][j][1] == false) {
-                            System.out.print(j + " - L \t");
+            if (andar == 2){
+                for (int i = 1; i < 21; i++){
+                    System.out.print (i + "A  ");
+                    for (int j = 1; j < 31; j++){
+                        if (assento [i][j][1] == false){
+                            System.out.print (j + " - L \t");
                         } else {
-                            System.out.print(j + " - V \t");
+                            System.out.print (j + " - V \t");
                         }
                     }
-                    System.out.println();
+                    System.out.println ();
                 }
             }
 
-            if (andar == 3) {
-                for (int i = 1; i < 11; i++) {
-                    System.out.print(i + "\t");
-                    for (int j = 1; j < 16; j++) {
-                        if (assento[i][j][2] == false) {
-                            System.out.print(j + " - L \t");
+            if (andar == 3){
+                for (int i = 1; i < 11; i++){
+                    System.out.print (i + "\t");
+                    for (int j = 1; j < 16; j++){
+                        if (assento [i][j][2] == false){
+                            System.out.print (j + " - L \t");
                         } else {
-                            System.out.print(j + " - V \t");
+                            System.out.print (j + " - V \t");
                         }
                     }
-                    System.out.println();
+                    System.out.println ();
                 }
             }
-            System.out.println();
-            Menu.menu();
+
+            if (andar == 1 || andar == 2 || andar == 3){
+                System.out.println ();
+                Menu.menu ();
+            }
+
         }
         while (andar != 1 && andar != 2 && andar != 3);
+
         Erros.andar_invalido ("Andar inválido");
     }
 
     public static void comprar (int andar, int linha, int coluna){
         if (assento [linha][coluna][andar] == false){
             assento [linha][coluna][andar] = true;
+            System.out.println ();
             System.out.println ("A compra do assento na coluna " + coluna + " na linha " + linha + " foi realizado com sucesso!");
             Logs.logs (andar,linha, coluna,"Compra");
         }
         else {
-            System.out.println ("O assento escohido já foi comprado!");
-            System.out.println ("Escolha outro assento!");
-            Compra.compra ();
+            Erros.poltrona_invalida ("Poltrona inválida");
+            Erros.compra_cadeira ("Compra de cadeira já comprada");
+            Menu.menu ();
         }
     }
 
     public static void cancelar (int andar, int linha, int coluna){
         if (assento [linha][coluna][andar] == true){
             assento [linha][coluna][andar] = false;
+            System.out.println ();
             System.out.println ("O cancelamento do assento na coluna " + coluna + " na linha " + linha + " foi realizado com sucesso!");
             Logs.logs (andar,linha, coluna,"Desistência");
         }
         else {
-            System.out.println ("Erro no cancelamento do assento!");
-            System.out.println ("Tente novamento!");
-            Desistencia.desistencia ();
+            Erros.desistencia_cadeira ("Desistência de cadeira livre");
+            Menu.menu ();
         }
     }
 }
