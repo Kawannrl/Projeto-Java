ad0a748584f18161c0f10d61be3efaf049e4b6e8
Kawannrl <165685845+Kawannrl@users.noreply.github.com>
2024-11-22 16:55:11 -0300

    Update Main.java

--- Atividade 18.java/Main.java
+++ Atividade 18.java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main (String [] args){
 
         System.out.println ("\n||     BEM VINDO     ||\n");
+        Erros.iniciar_sistema ("Inicialização do sistema");
         Menu.menu ();
     }
 }
