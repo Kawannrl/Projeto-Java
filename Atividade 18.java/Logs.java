3d71385ebb8802022739108375f72ef2cc14f663
Kawannrl <165685845+Kawannrl@users.noreply.github.com>
2024-11-22 16:57:29 -0300

    Update Logs.java

--- Atividade 18.java/Logs.java
+++ Atividade 18.java/Logs.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class Logs {
     static String contador [][] = new String [500][3];
@@ -6,21 +7,28 @@ public class Logs {
         int i = 0;
         while (true){
             if (null == contador [i][0]){
-                contador [i][0] = "a " + Integer.toString (andar) + " | l " + Integer.toString (linha) + " | c " + Integer.toString (coluna);
+                contador [i][0] = "a " + Integer.toString (andar + 1) + " | l " + Integer.toString (linha) + " | c " + Integer.toString (coluna);
                 contador [i][1] = Status;
-                contador [i][2] = String.valueOf(data());
+                contador [i][2] = Logs.data ();
                 break;
             }
             i++;
         }
     }
-    public static Date data (){
+
+    public static String data (){
         Date Agora = new Date ();
-        return Agora;
+        String data = new SimpleDateFormat ("dd/MM/yyyy  ").format (Agora);
+        String hora = new SimpleDateFormat ("HH:mm:ss  ").format (Agora);
+
+        return data+"  "+hora;
     }
+
     public static void imprimir (){
 
+        System.out.println ();
         System.out.println ("Histórico de compra: ");
+
         for (int i = 0; i < contador.length; i++){
             if (contador [i][0] == null){
                 break;
