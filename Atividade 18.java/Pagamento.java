4371f01924466d95bf18168b69fefb56d0fc2c44
Kawannrl <165685845+Kawannrl@users.noreply.github.com>
2024-11-27 17:18:31 -0300

    Update Pagamento.java

--- Atividade 18.java/Pagamento.java
+++ Atividade 18.java/Pagamento.java
@@ -1,13 +1,13 @@
-import java.util.Scanner;
-
 public class Pagamento {
+    public static double dinheiro = 0.0;
+    public static double troco = 0.0;
+    public static double total = 0.0;
     public static void pagamento (int andar, int linha, int coluna, String Status){
-        Scanner scanner = new Scanner (System.in);
 
         int forma_pagamento;
 
         do {
-            System.out.println ("Informe a forma de pagamento: \n1 - Pix\n2 - Cartão de Débito\n3 - Cartão de Crédito\n4 - Dinheiro");
+            System.out.println ("\nInforme a forma de pagamento: \n1 - Pix\n2 - Cartão de Débito\n3 - Cartão de Crédito\n4 - Dinheiro");
             System.out.print ("Resposta: ");
             forma_pagamento = Entrada.entrada_pagamento ();
 
@@ -36,22 +36,33 @@ public class Pagamento {
 
     public static void pagamento_pix (int andar, int linha, int coluna, String Status){
 
-        System.out.println ("Você selecionou o Pix como forma de pagamento!");
-        System.out.println ("Escaneie o QR Code abaixo para o pagamento");
+        dinheiro = 20;
+        total += dinheiro;
+
+        System.out.println ("\nVocê selecionou o Pix como forma de pagamento!");
+        System.out.println ("Escaneie a chave Pix abaixo para o pagamento");
+        System.out.println ("(41)99924-8834");
+        Logs.valor_teatro ("Compra", "Pix", dinheiro, troco, total);
+        Logs.logs (andar,linha, coluna,"Comprar", "Pix");
     }
 
     public static void pagamento_debito (int andar, int linha, int coluna, String Status){
 
         int senha;
 
-        System.out.println ("Você selecionou o Cartão de Débito como forma de pagamento!");
+        dinheiro = 20;
+        total += dinheiro;
+
+        System.out.println ("\nVocê selecionou o Cartão de Débito como forma de pagamento!");
         System.out.print ("Senha: ");
 
         senha = Entrada.entrada_pagamento_debito ();
 
         if (senha == 1234){
             System.out.println ("Pagamento realizado com sucesso!");
-            Logs.logs (andar,linha, coluna,"Compra", "Cartão de Débito");
+            System.out.println ();
+            Logs.valor_teatro ("Compra", "Cartão de Débito", dinheiro, troco, total);
+            Logs.logs (andar,linha, coluna,"Comprar", "Cartão de Débito");
         }
     }
 
@@ -59,19 +70,51 @@ public class Pagamento {
 
         int senha;
 
-        System.out.println ("Você selecionou o Cartão de Crédito como forma de pagamento!");
+        dinheiro = 20;
+        total += dinheiro;
+
+        System.out.println ("\nVocê selecionou o Cartão de Crédito como forma de pagamento!");
         System.out.print ("Senha: ");
 
         senha = Entrada.entrada_pagamento_credito ();
 
         if (senha == 1234){
             System.out.println ("Pagamento realizado com suceso!");
+            System.out.println ();
+            Logs.valor_teatro ("Compra", "Cartão de Crédito", dinheiro, troco, total);
             Logs.logs (andar,linha, coluna,"Compra", "Cartão de Crédito");
         }
     }
 
     public static void pagamento_dinheiro (int andar, int linha, int coluna, String Status){
 
-        System.out.println ("Você selecionou o Dinheiro como forma de pagamento!");
+        System.out.println ("\nVocê selecionou o Dinheiro como forma de pagamento!");
+        System.out.println ();
+        System.out.println ("Informe o valor do pagamento");
+        System.out.print ("Resposta: ");
+
+        dinheiro = Entrada.entrada_pagamento_dinheiro ();
+
+        if (dinheiro > 20){
+            total += dinheiro;
+            troco = dinheiro - 20;
+
+            System.out.println ("Pagamento realizado com sucesso");
+            System.out.println ("Troco do pagamento: " + troco);
+            System.out.println ();
+            Logs.valor_teatro ("Compra", "Dinheiro", dinheiro, troco, total);
+            Logs.logs (andar,linha, coluna,"Compra", "Dinheiro");
+        }
+        else if (dinheiro == 20){
+            System.out.println ("Pagamento realizado com sucesso");
+            Logs.valor_teatro ("Compra", "Dinheiro", dinheiro, troco, total);
+            Logs.logs (andar,linha, coluna,"Compra", "Dinheiro");
+        }
+        else {
+            System.out.println ("Erro!!!");
+            System.out.println ("Falha no pagamento");
+            System.out.println ("Tente novamente");
+            Compra.compra ();
+        }
     }
 }
