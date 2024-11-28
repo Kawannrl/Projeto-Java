30f6a800264ee617abea615794a452e31b6171bb
Kawannrl <165685845+Kawannrl@users.noreply.github.com>
2024-11-28 15:33:21 -0300

    Update Pagamento.java

--- Atividade 18.java/Pagamento.java
+++ Atividade 18.java/Pagamento.java
@@ -7,7 +7,7 @@ public class Pagamento {
         int forma_pagamento;
 
         do {
-            System.out.println ("\nInforme a forma de pagamento: \n1 - Pix\n2 - Cartão de Débito\n3 - Cartão de Crédito\n4 - Dinheiro");
+            System.out.println ("\nEscolha uma das opções: \n1 - Pix\n2 - Cartão de Débito\n3 - Cartão de Crédito\n4 - Dinheiro");
             System.out.print ("Resposta: ");
             forma_pagamento = Entrada.entrada_pagamento ();
 
@@ -36,85 +36,152 @@ public class Pagamento {
 
     public static void pagamento_pix (int andar, int linha, int coluna, String Status){
 
-        dinheiro = 20;
-        total += dinheiro;
+        if (Status == "Comprar"){
 
-        System.out.println ("\nVocê selecionou o Pix como forma de pagamento!");
-        System.out.println ("Escaneie a chave Pix abaixo para o pagamento");
-        System.out.println ("(41)99924-8834");
-        Logs.valor_teatro ("Compra", "Pix", dinheiro, troco, total);
-        Logs.logs (andar,linha, coluna,"Comprar", "Pix");
+            dinheiro = 20;
+            total += dinheiro;
+
+            System.out.println ("\nVocê selecionou o Pix como forma de pagamento!");
+            System.out.println ("Escaneie a chave Pix abaixo para o pagamento");
+            System.out.println ("(41)99924-8834");
+            Logs.valor_teatro ("Compra", "Pix", dinheiro, troco, total);
+            Logs.logs (andar, linha, coluna, "Comprar", "Pix");
+        }
+
+        if (Status == "Desistência"){
+
+            dinheiro = -20;
+            total += dinheiro;
+
+            System.out.println ("\nVocê selecionou o Pix como forma de devolução!");
+            System.out.println ("Devolução realizada com sucesso!");
+            System.out.println ();
+            Logs.valor_teatro ("Desistência", "Pix", dinheiro, troco, total);
+            Logs.logs (andar, linha, coluna, "Desistência", "Pix");
+        }
     }
 
     public static void pagamento_debito (int andar, int linha, int coluna, String Status){
 
         int senha;
 
-        dinheiro = 20;
-        total += dinheiro;
-
         System.out.println ("\nVocê selecionou o Cartão de Débito como forma de pagamento!");
         System.out.print ("Senha: ");
 
         senha = Entrada.entrada_pagamento_debito ();
 
-        if (senha == 1234){
-            System.out.println ("Pagamento realizado com sucesso!");
-            System.out.println ();
-            Logs.valor_teatro ("Compra", "Cartão de Débito", dinheiro, troco, total);
-            Logs.logs (andar,linha, coluna,"Comprar", "Cartão de Débito");
+        if (Status == "Comprar"){
+            if (senha == 1234){
+
+                dinheiro = 20;
+                total += dinheiro;
+
+                System.out.println ("Pagamento realizado com sucesso!");
+                System.out.println ();
+                Logs.valor_teatro ("Compra", "Cartão de Débito", dinheiro, troco, total);
+                Logs.logs (andar, linha, coluna, "Comprar", "Cartão de Débito");
+            }
+        }
+
+        if (Status == "Desistência"){
+
+            if (senha == 1234){
+
+                dinheiro = -20;
+                total += dinheiro;
+
+                System.out.println ("Devolução realizada com sucesso!");
+                System.out.println ();
+                Logs.valor_teatro ("Desistência", "Cartão de Débito", dinheiro, troco, total);
+                Logs.logs (andar, linha, coluna, "Desistência", "Cartão de Débito");
+            }
         }
     }
+
+
 
     public static void pagamento_credito (int andar, int linha, int coluna, String Status){
 
         int senha;
-
-        dinheiro = 20;
-        total += dinheiro;
 
         System.out.println ("\nVocê selecionou o Cartão de Crédito como forma de pagamento!");
         System.out.print ("Senha: ");
 
         senha = Entrada.entrada_pagamento_credito ();
 
-        if (senha == 1234){
-            System.out.println ("Pagamento realizado com suceso!");
-            System.out.println ();
-            Logs.valor_teatro ("Compra", "Cartão de Crédito", dinheiro, troco, total);
-            Logs.logs (andar,linha, coluna,"Compra", "Cartão de Crédito");
+        if (Status == "Comprar"){
+            if (senha == 1234){
+
+                dinheiro = 20;
+                total += dinheiro;
+
+                System.out.println ("Pagamento realizado com suceso!");
+                System.out.println ();
+                Logs.valor_teatro ("Compra", "Cartão de Crédito", dinheiro, troco, total);
+                Logs.logs (andar, linha, coluna, "Compra", "Cartão de Crédito");
+            }
+        }
+
+        if (Status == "Desistência"){
+            if (senha == 1234){
+
+                dinheiro = -20;
+                total += dinheiro;
+
+                System.out.println ("Devolução realizada com suceso!");
+                System.out.println ();
+                Logs.valor_teatro ("Desistência", "Cartão de Crédito", dinheiro, troco, total);
+                Logs.logs (andar, linha, coluna, "Desistência", "Cartão de Crédito");
+            }
         }
     }
 
     public static void pagamento_dinheiro (int andar, int linha, int coluna, String Status){
 
-        System.out.println ("\nVocê selecionou o Dinheiro como forma de pagamento!");
-        System.out.println ();
-        System.out.println ("Informe o valor do pagamento");
-        System.out.print ("Resposta: ");
+        if (Status == "Comprar"){
 
-        dinheiro = Entrada.entrada_pagamento_dinheiro ();
-
-        if (dinheiro > 20){
-            total += dinheiro;
-            troco = dinheiro - 20;
-
-            System.out.println ("Pagamento realizado com sucesso");
-            System.out.println ("Troco do pagamento: " + troco);
+            System.out.println ("\nVocê selecionou o Dinheiro como forma de pagamento!");
             System.out.println ();
-            Logs.valor_teatro ("Compra", "Dinheiro", dinheiro, troco, total);
-            Logs.logs (andar,linha, coluna,"Compra", "Dinheiro");
+            System.out.println ("Informe o valor do pagamento");
+            System.out.print ("Resposta: ");
+
+            dinheiro = Entrada.entrada_pagamento_dinheiro ();
+
+            if (dinheiro > 20){
+
+                total += dinheiro;
+                troco = dinheiro - 20;
+
+                System.out.println ("Pagamento realizado com sucesso");
+                System.out.println ("Troco do pagamento: " + troco);
+                System.out.println ();
+                Logs.valor_teatro ("Compra", "Dinheiro", dinheiro, troco, total);
+                Logs.logs (andar, linha, coluna, "Compra", "Dinheiro");
+            }
+            else if (dinheiro == 20){
+                System.out.println ("Pagamento realizado com sucesso");
+                Logs.valor_teatro ("Compra", "Dinheiro", dinheiro, troco, total);
+                Logs.logs (andar, linha, coluna, "Compra", "Dinheiro");
+            }
+            else {
+                System.out.println ("Erro!!!");
+                System.out.println ("Falha no pagamento");
+                System.out.println ("Tente novamente");
+                Compra.compra ();
+            }
         }
-        else if (dinheiro == 20){
-            System.out.println ("Pagamento realizado com sucesso");
-            Logs.valor_teatro ("Compra", "Dinheiro", dinheiro, troco, total);
-            Logs.logs (andar,linha, coluna,"Compra", "Dinheiro");
-        }
-        else {
-            System.out.println ("Erro!!!");
-            System.out.println ("Falha no pagamento");
-            System.out.println ("Tente novamente");
-            Compra.compra ();
+
+        if (Status == "Desistência"){
+
+            dinheiro = -20;
+            total += dinheiro;
+
+            System.out.println ("Você selecionou o Dinheiro como forma de devolução!");
+            System.out.println ();
+            System.out.println ("Dinheiro devolvido com sucesso");
+            System.out.println ();
+            Logs.valor_teatro ("Desistência", "Dinheiro", dinheiro, troco, total);
+            Logs.logs (andar, linha, coluna, "Desistência", "Dinheiro");
         }
     }
 }
